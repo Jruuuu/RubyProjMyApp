d6a026745ca1f7b7847aba370145cd0f32770495
Enrilo Ugalde <ugalde_enrilo@bah.com>
2022-07-14 12:40:43 -0700

    added validations to email and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+                      uniqueness: {case_sensitive: false},
+                      length: {minimum: 3, maximum: 25}
+
+  VALID_EMAIL_REGEX =/\A[a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/
+  
+  validates :email, presence: true,
+                    uniqueness: {case_sensitive: false},
+                    length: {maximum: 105}, 
+                    format: {with: VALID_EMAIL_REGEX }
 end
