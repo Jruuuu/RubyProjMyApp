67a367465c1d658c32740d874b03d39a0dbfb258
Enrilo Ugalde <ugalde_enrilo@bah.com>
2022-07-14 13:12:56 -0700

    added on to many assocations between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   # POST /articles
   def create
     @article = Article.new(article_params)
-
+    @article.user = User.first
     if @article.save
       redirect_to @article, notice: "Article was successfully created."
     else
